ec1216507c2487acf5ceb4974bb4a5851c47d184
vidhyanamini10 <124749085+vidhyanamini10@users.noreply.github.com>
2024-01-10 21:39:56 -0600

    created new var and return
    created a new variable and assigining value to it and retuning the variable

---
+++ Student.java
@@ -0,0 +1,11 @@
+class Student{ // defining a class in java
+    String name = "srividhya"; // creating string variable and assigining value
+
+
+        String getName(){  // creating an method which will returns the name
+            return name;
+
+        }
+    
+
+}
